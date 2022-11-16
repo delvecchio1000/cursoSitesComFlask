10b23fbd3436db049d863002620835552271d757
delvecchio1000 <delvecchio1000@gmail.com>
2022-11-15 23:39:54 -0300

    Bloqueando páginas para usuários visitantes; Redirecionamento automático inteligente; Página de Perfil - html e css; imagem do perfil logado
    Adicionado @login_required, Bloqueando páginas para usuários visitantes
    
    Adicionado par_next = request.args.get('next')  # par_next (leia-se parâmetro next, que é a pagina que o usuário queria acessar antes do login). Redirecionamento automático inteligente
    
    Editando a Página de Perfil - html e css
    
    Colocando a imagem do perfil logado

--- SiteComunidade/comunidadeimpressionadora/__init__.py
+++ SiteComunidade/comunidadeimpressionadora/__init__.py
@@ -6,15 +6,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+#from datetime import timedelta
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '3ac9b3a5750f8a5960cff8f039aacca3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+#app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_maneger = LoginManager(app)
+login_maneger.login_view = 'login'
+login_maneger.login_message_category = 'alert-info'
 
 
 from comunidadeimpressionadora import routes
--- SiteComunidade/comunidadeimpressionadora/routes.py
+++ SiteComunidade/comunidadeimpressionadora/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, redirect, url_for, flash, request
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuario
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -18,6 +18,7 @@ def contato():
     return render_template('contato.html')
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
@@ -31,7 +32,11 @@ def login():
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-            return redirect(url_for('home'))
+            par_next = request.args.get('next')  # par_next (leia-se parâmetro next, que é a pagina que o usuário queria acessar antes do login)
+            if par_next:
+                return  redirect(par_next)
+            else:
+                return redirect(url_for('home'))
         else:
             flash(f'Falha no Login. E-mail ou Senha incorretos', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
@@ -48,15 +53,19 @@ def login():
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 @app.route('/sair')
+@login_required
 def sair():
     logout_user()
     flash(f'Logout feito com sucesso', 'alert-success')
     return redirect(url_for('home'))
 
 @app.route('/perfil')
+@login_required
 def perfil():
-    return render_template('perfil.html')
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
 
 @app.route('/post/criar')
+@login_required
 def criar_post():
     return render_template('criarpost.html')
